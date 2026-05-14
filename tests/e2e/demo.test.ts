9a1966780e8b51f97f1c29d0098f496cef17d8f5
Nabh Khanna <73828621+nkgastromatic@users.noreply.github.com>
2026-05-14 16:25:49 +0200

    chore: add vitest test setup (#2)
    Co-authored-by: nkgastromatic <n.khanna@gastromatic.com>

---
+++ tests/e2e/demo.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('demo e2e test', () => {
+  it('passes', () => {
+    expect('e2e').toBe('e2e');
+  });
+});
---
+++ tests/unit/demo.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('demo unit test', () => {
+  it('passes', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
